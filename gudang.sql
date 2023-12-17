7b06bebb1a16c4ab3fd6b1da19e45b8192b88630
fahrulsheva03 <90319974+fahrulsheva03@users.noreply.github.com>
2023-12-17 15:39:33 +0700

    SQL

---
+++ gudang.sql
@@ -0,0 +1,306 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 17, 2023 at 09:38 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gudang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `telepon` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id_admin`, `username`, `password`, `nama`, `telepon`, `foto`) VALUES
+(12, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', '0812838281', '22092020020607employee1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_ajuan`
+--
+
+CREATE TABLE `tb_ajuan` (
+  `no_ajuan` int(11) NOT NULL,
+  `tanggal` varchar(255) NOT NULL,
+  `periode` varchar(50) NOT NULL,
+  `kode_brg` varchar(11) NOT NULL,
+  `nama_brg` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `jml_ajuan` int(11) NOT NULL,
+  `petugas` varchar(255) NOT NULL,
+  `val` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_ajuan`
+--
+
+INSERT INTO `tb_ajuan` (`no_ajuan`, `tanggal`, `periode`, `kode_brg`, `nama_brg`, `stok`, `jml_ajuan`, `petugas`, `val`) VALUES
+(142412424, '2023-12-11', '06	', '92938', 'Ssd IOS 1 tb', 91, 2, 'Petugas', 1),
+(142412425, '2023-12-11', '08	', '88821', 'RAM DDR10 1024 GB', 3142, 2, 'Petugas', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `kode_brg` int(11) NOT NULL,
+  `nama_brg` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `rak` varchar(255) NOT NULL,
+  `supplier` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`kode_brg`, `nama_brg`, `stok`, `rak`, `supplier`) VALUES
+(88821, 'RAM DDR10 1024 GB', 3142, 'RAK 002\r\n', 'pp'),
+(92938, 'Ssd IOS 1 tb', 91, 'RAK 001', 'CV.Matahari'),
+(99101, 'Flashdisk 12 gb', 23, 'RAK 001', 'CV.Abadi Sentosa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang_in`
+--
+
+CREATE TABLE `tb_barang_in` (
+  `id_brg_in` int(11) NOT NULL,
+  `tanggal` varchar(255) NOT NULL,
+  `noinv` varchar(11) NOT NULL,
+  `supplier` varchar(255) NOT NULL,
+  `kode_brg` varchar(11) NOT NULL,
+  `nama_brg` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `jml_masuk` int(11) NOT NULL,
+  `jam` varchar(255) NOT NULL,
+  `petugas` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_barang_in`
+--
+
+INSERT INTO `tb_barang_in` (`id_brg_in`, `tanggal`, `noinv`, `supplier`, `kode_brg`, `nama_brg`, `stok`, `jml_masuk`, `jam`, `petugas`) VALUES
+(11, '2023-11-10', '241231', 'pp', '88821', 'RAM DDR10 1024 GB', 11, 3131, '03:30 am', 'Petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang_out`
+--
+
+CREATE TABLE `tb_barang_out` (
+  `no_brg_out` int(11) NOT NULL,
+  `no_ajuan` int(11) NOT NULL,
+  `tanggal_ajuan` varchar(255) NOT NULL,
+  `tanggal_out` varchar(255) NOT NULL,
+  `petugas` varchar(255) NOT NULL,
+  `kode_brg` varchar(11) NOT NULL,
+  `nama_brg` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `jml_ajuan` int(11) NOT NULL,
+  `jml_keluar` int(11) NOT NULL,
+  `admin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_barang_out`
+--
+
+INSERT INTO `tb_barang_out` (`no_brg_out`, `no_ajuan`, `tanggal_ajuan`, `tanggal_out`, `petugas`, `kode_brg`, `nama_brg`, `stok`, `jml_ajuan`, `jml_keluar`, `admin`) VALUES
+(21421, 142412421, '2023-11-06', '2023-11-10', 'Petugas', '--- PILIH -', 'RAM DDR10 1024 GB', 11, 21, 200, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_petugas`
+--
+
+CREATE TABLE `tb_petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `telepon` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_petugas`
+--
+
+INSERT INTO `tb_petugas` (`id_petugas`, `username`, `password`, `nama`, `telepon`) VALUES
+(21, 'petugas', 'afb91ef692fd08c445e8cb1bab2ccf9c', 'Petugas', '081255532987'),
+(22, 'Herlin', 'd34fb357dde0fe2a7aa0687c9a855fd2', 'Herlin', '42343213123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rak`
+--
+
+CREATE TABLE `tb_rak` (
+  `id_rak` int(11) NOT NULL,
+  `nama_rak` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_rak`
+--
+
+INSERT INTO `tb_rak` (`id_rak`, `nama_rak`) VALUES
+(1, 'RAK 001'),
+(2, 'RAK 002\r\n'),
+(3, 'RAK 003');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_sup`
+--
+
+CREATE TABLE `tb_sup` (
+  `id_sup` int(11) NOT NULL,
+  `nama_sup` varchar(255) NOT NULL,
+  `kontak_sup` varchar(255) NOT NULL,
+  `alamat_sup` text NOT NULL,
+  `telepon_sup` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_sup`
+--
+
+INSERT INTO `tb_sup` (`id_sup`, `nama_sup`, `kontak_sup`, `alamat_sup`, `telepon_sup`) VALUES
+(6, 'pp', 'pp', 'pp', '009');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tb_ajuan`
+--
+ALTER TABLE `tb_ajuan`
+  ADD PRIMARY KEY (`no_ajuan`);
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`kode_brg`);
+
+--
+-- Indexes for table `tb_barang_in`
+--
+ALTER TABLE `tb_barang_in`
+  ADD PRIMARY KEY (`id_brg_in`);
+
+--
+-- Indexes for table `tb_barang_out`
+--
+ALTER TABLE `tb_barang_out`
+  ADD PRIMARY KEY (`no_brg_out`);
+
+--
+-- Indexes for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `tb_rak`
+--
+ALTER TABLE `tb_rak`
+  ADD PRIMARY KEY (`id_rak`);
+
+--
+-- Indexes for table `tb_sup`
+--
+ALTER TABLE `tb_sup`
+  ADD PRIMARY KEY (`id_sup`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tb_ajuan`
+--
+ALTER TABLE `tb_ajuan`
+  MODIFY `no_ajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=142412426;
+
+--
+-- AUTO_INCREMENT for table `tb_barang_in`
+--
+ALTER TABLE `tb_barang_in`
+  MODIFY `id_brg_in` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `tb_rak`
+--
+ALTER TABLE `tb_rak`
+  MODIFY `id_rak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tb_sup`
+--
+ALTER TABLE `tb_sup`
+  MODIFY `id_sup` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
